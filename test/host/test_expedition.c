d920ddf4062cafeb136e4beb40d24e9e994d92ee
willem <willem@alasia.co.za>
2026-09-12 15:02:26 +0200

    feat: expedition logic - start/done/remaining/resolve (v0.3 Task 2)

--- test/host/test_expedition.c
+++ test/host/test_expedition.c
@@ -9,3 +9,33 @@ void run_expedition_defaults_tests(void) {
     CHECK(s.persistent.expedition_minutes == 0);
     CHECK(s.journey_ready == 0);
 }
+
+#include "expedition.h"
+#include "hunt.h"
+#include "rng_control.h"
+void run_expedition_logic_tests(void) {
+    char log[96];
+    rng_seed(3);
+    struct GameState s = {0}; game_state_init(&s, 0);
+    expedition_start(&s, EXPED_SHORT_MIN, 1000);
+    CHECK(s.persistent.on_expedition == 1);
+    CHECK(expedition_done(&s, 1000 + EXPED_SHORT_MIN*EXPED_SEC_PER_MIN) == true);
+    CHECK(expedition_done(&s, 1000 + EXPED_SHORT_MIN*EXPED_SEC_PER_MIN - 1) == false);
+    CHECK(expedition_remaining_sec(&s, 1000) == EXPED_SHORT_MIN*EXPED_SEC_PER_MIN);
+
+    // epic yields more hoard than short (averaged)
+    uint32_t hs=0, he=0;
+    for(int i=0;i<40;i++){ rng_seed(100+i); struct GameState a={0}; game_state_init(&a,0);
+        a.persistent.on_expedition=1; a.persistent.expedition_minutes=EXPED_SHORT_MIN;
+        expedition_resolve(&a,log,sizeof(log)); hs+=a.persistent.hoard; }
+    for(int i=0;i<40;i++){ rng_seed(100+i); struct GameState a={0}; game_state_init(&a,0);
+        a.persistent.on_expedition=1; a.persistent.expedition_minutes=EXPED_EPIC_MIN;
+        expedition_resolve(&a,log,sizeof(log)); he+=a.persistent.hoard; }
+    CHECK(he > hs);
+
+    // resolve clears flag and never kills
+    struct GameState r={0}; game_state_init(&r,0);
+    r.persistent.on_expedition=1; r.persistent.expedition_minutes=EXPED_EPIC_MIN; r.persistent.health=3;
+    for(int i=0;i<50;i++){ rng_seed(i); struct GameState x=r; expedition_resolve(&x,log,sizeof(log));
+        CHECK(x.persistent.on_expedition==0); CHECK(x.persistent.health>=1); }
+}
--- test/host/tests.h
+++ test/host/tests.h
@@ -13,4 +13,5 @@ void run_hunt_logic_tests(void);
 void run_economy_tests(void);
 void run_inventory_tests(void);
 void run_expedition_defaults_tests(void);
+void run_expedition_logic_tests(void);
 void run_hunt_hw_tests(void);
